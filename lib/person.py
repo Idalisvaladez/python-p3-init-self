c1845ff5cb94a9b28ada8c2e03432d96c08bc602
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-12 20:46:18 -0500

    Complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    
+    #Initialize : creates working copy of Person class with __init__
+    #creates instance or object that we can assign attributes to
+    #self is reference to the currect instance of the class and is used
+    #to access its attributes
+    def __init__(self, name):
+        self.name = name
